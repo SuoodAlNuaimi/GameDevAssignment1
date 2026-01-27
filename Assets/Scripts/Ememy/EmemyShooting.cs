b013156824be221a3807b4c191df5efb93f1fe52
SuoodAlNuaimi <suoodalnuaimi28@gmail.com>
2026-01-27 20:19:26 +0400

    Distance

--- Assets/Scripts/Ememy/EmemyShooting.cs
+++ Assets/Scripts/Ememy/EmemyShooting.cs
@@ -8,22 +8,34 @@ public class EmemyShooting : MonoBehaviour
     public Transform bulletPos;
 
     private float timer;
+    private GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("KnightPlayer");
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
 
-        if (timer>2)
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+        Debug.Log(distance);
+
+        if (distance<4)
         {
-            timer = 0;
-            shoot(); 
+            timer += Time.deltaTime;
+
+            if (timer>2)
+            {
+                 timer = 0;
+                shoot(); 
+            }
+
+
         }
+
+        
     }
 
     void shoot()
