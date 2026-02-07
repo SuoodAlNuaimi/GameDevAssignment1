4bcd1e4aa1facc009f84f357383c03c64f3c9b4c
SuoodAlNuaimi <suoodalnuaimi28@gmail.com>
2026-02-08 02:51:19 +0400

    MainMenu

---
+++ Assets/Scripts/Core/MainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayHardGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void PlayEasyGame()
+    {
+        SceneManager.LoadScene("EasyMode");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
